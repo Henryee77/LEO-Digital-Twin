aabc027b9a60991e25a640d90f76495ea06af278
Henryee77 <henry9685@gmail.com>
2024-07-19 16:28:54 +0800

    add docstring in distribution.py

--- low_earth_orbit/util/distribution.py
+++ low_earth_orbit/util/distribution.py
@@ -9,35 +9,6 @@ from scipy.stats import rayleigh, nakagami, uniform, norm
 from scipy.special import hyp1f1
 
 
-class Shadowed_Rician(rv_continuous):
-  """The shadowed rician distribution, 
-  which is used in the fading channel of LEO to ground.
-  """
-
-  # , LOS_power: float, NLOS_power: float, m: float
-  def _pdf(self, x: float, LOS_power: float, NLOS_power: float, m: float) -> float:
-    """The PDF of the shadowed Rician fading.
-
-    Args:
-      x (float): input
-      LOS_power (float): The average power of the LOS component.
-      NLOS_power (float): The average power of the multipath component.
-      m (float): Nakagami parameter.
-
-
-    Returns:
-      float: The probability of x.
-    """
-    # print(args, type(args))
-    # (LOS_power, NLOS_power, m) = args
-    NLOS_ratio = NLOS_power * m / (NLOS_power * m + LOS_power)
-    alpha = 1 / NLOS_power * (NLOS_ratio ** m)
-    beta = 1 / NLOS_power
-    delta = (NLOS_power ** 2 / 2 * m + NLOS_power / 2 * LOS_power) * LOS_power / 2
-    print(alpha, math.exp(-beta * x), hyp1f1(m, 1, delta * x))
-    return alpha * math.exp(-beta * x) * hyp1f1(m, 1, delta * x)
-
-
 class Rayleigh():
   """Rayleigh Distribution"""
 
@@ -46,10 +17,27 @@ class Rayleigh():
 
   @overload
   def rvs(self, scale: float) -> float:
+    """Generate the Rayleigh random number.
+
+    Args:
+        scale (float): The scale parameter (σ) of the rayleigh distribution.
+
+    Returns:
+        float: The generated number.
+    """
     ...
 
   @overload
   def rvs(self, scale: float, size: int) -> npt.NDArray[np.float64]:
+    """Generate the Rayleigh random number.
+
+    Args:
+        scale (float): The scale parameter (σ) of the rayleigh distribution.
+        size (int): The total number of generated numbers.
+
+    Returns:
+        npt.NDArray[np.float64]: The array of generated number.
+    """
     ...
 
   def rvs(self, scale, size=None):
@@ -67,10 +55,29 @@ class Nakagami():
 
   @overload
   def rvs(self, nu: float, scale: float) -> float:
+    """Generate the Nakagami random number.
+
+    Args:
+        nu (float): The Nakagami parameter (m).
+        scale (float): The scale parameter (Ω) of the rayleigh distribution.
+
+    Returns:
+        float: The generated number.
+    """
     ...
 
   @overload
   def rvs(self, nu: float, scale: float, size: int) -> npt.NDArray[np.float64]:
+    """Generate the Nakagami random number.
+
+    Args:
+        nu (float): The Nakagami parameter (m).
+        scale (float): The scale parameter (Ω) of the rayleigh distribution.
+        size (int): The total number of generated numbers.
+
+    Returns:
+        npt.NDArray[np.float64]: The array of generated number.
+    """
     ...
 
   def rvs(self, nu, scale, size=None):
